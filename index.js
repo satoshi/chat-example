96ebf35ff8a1736650c39e455ff93cb1b9541ad0
satoshi <satoshi.yagi@yahoo.com>
2016-09-10 15:15:04 -0700

    tabs must die

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
-	socket.on('chat message', function(msg) {
-		io.emit('chat message', msg);
-	});
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(3000, function() {
-	console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
